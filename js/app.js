89161ffd4822775fb946f7fc925935caf5af7d0f
NaszAkos <naszakos@turr.hu>
2024-06-06 14:15:29 +0200

    js_alapok

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let emailFld = document.getElementById('emailFld').value;
+let passwdFld = document.getElementById('passwdField').value;
+let loginBtn = document.getElementById('loginBtn').value;
+
+console.log(emailFld)
